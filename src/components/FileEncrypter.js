7040075e87f3b86b3658c843041709926217f232
JustinPaulJohnUA <justinpauljohn@email.arizona.edu>
2021-03-09 23:43:20 -0700

    progress made on encryption applet. file upload, encryption, and decryption working mostly. progress made on transitions between applet steps made too.

--- src/components/FileEncrypter.js
+++ src/components/FileEncrypter.js
@@ -1,205 +1,168 @@
-import React, { useState, useRef } from 'react'
-
-/* MATERIAL-UI IMPORTS*/
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import MobileStepper from '@material-ui/core/MobileStepper';
-import Button from '@material-ui/core/Button';
-import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
-import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
-import TextField from '@material-ui/core/TextField';
-
-import CryptoJS from 'crypto-js'
-import CryptoAES from 'crypto-js/aes';
+import React, { useRef } from 'react'
 
 /*SYTLE IMPORTS*/
 import '../styles/FileEncrypter.css'
 
 /*COMPONENT IMPORTS */
-
+import CryptoJS from 'crypto-js'
 
 export default function FileEncrypter() {
+    // TOP NAVIGATION
+    var passwordNavRef = useRef();
+    var uploadNavRef = useRef();
 
-    const [activeStep, setActiveStep] = useState(0);
-    const [fileURI, setFileURI] = useState("");
-    const [encryptResult, setEncryptResult] = useState("");
-    const [decryptResult, setDecryptResult] = useState("");
+    var currentStep = 'password-input-wrapper';
+    var availableNavs = [];
 
-    const optionSelectRef = useRef();
+    var password = '';
+    var fileObj = '';
+    var fileURL = '';
+    var encrypted = '';
+    var decrypted = '';
 
-    const encryptionOptionsRef = useRef();
-    const decryptionOptionsRef = useRef();
+    const reader = new FileReader();
 
-    const browseFileEncryptRef = useRef();
-    const browseFileDecryptRef = useRef();
-
-    const makeSecretKeyRef = useRef();
-    const enterSecretKeyRef = useRef();
-
-    const encryptStep1Ref = useRef();
-    const encryptStep2Ref = useRef();
-    const encryptStep3Ref = useRef()
-    const encryptStep3AnchorRef = useRef();
-
-    const decryptStep1Ref = useRef();
-    const decryptStep2Ref = useRef();
-    const decryptStep3Ref = useRef();
-    const decryptStep3AnchorRef = useRef();
-
-    var encryptedFileResult = "";
-    var decryptedFileResult = "";
-
-
-    function handleEncryptButton() {
-        const optionSelect = optionSelectRef.current
-        const encryptionOptions = encryptionOptionsRef.current;
-        const encryptStep3 = encryptStep3Ref.current;
-
-        optionSelect.style.display = "none";
-        encryptStep3.style.display = "none";
-        encryptionOptions.style.display = "block";
+    function setPassword(e) {
+        password = String(e.target.value)
+        if (password.length > 0) {
+            showTopNavigation(['upload-nav'])
+            showBottomNavigation('next')
+        } else {
+            showTopNavigation([])
+            showBottomNavigation('none')
+        }
+        
     }
 
-    function handleDecryptButton() {
-        const optionSelect = optionSelectRef.current
-        const decryptionOptions = decryptionOptionsRef.current
-        const decryptStep3 = decryptStep3Ref.current;
-
-        optionSelect.style.display = "none";
-        decryptStep3.style.display = "none"
-        decryptionOptions.style.display = "block"
+    function showTopNavigation(navs) {
+        if (navs.length === 0) {
+            console.log('nav length is 0')
+            var allnavs = ['upload-nav', 'encrypt-nav', 'decrypt-nav', 'download-nav']
+            allnavs.map(id => {
+                var navItem = document.getElementById(id);
+                var className = navItem.classList
+                if (!className.contains('disabled')) {
+                    navItem.classList.add('disabled')
+                    navItem.classList.remove('active')
+                }
+                
+            })
+        } else {
+            navs.map(id => {
+                var navItem = document.getElementById(id)
+                var className = navItem.classList;
+                if (!className.contains('active')) {
+                    className.remove('disabled')
+                    className.toggle('active')
+                } else if (className.contains('disabled')) {
+                    className.toggle('disabled')
+                    className.remove('active')
+                }
+                
+            })
+        }
 
 
     }
 
-    function handleEncryption(e) {
-        const secretKey = makeSecretKeyRef.current.value
+    function showBottomNavigation(decision) {
+        var bottomNavDiv = document.getElementById('show-steps-wrapper');
+        var showNext = document.getElementById('show-next-button');
+        var showPrev = document.getElementById('show-previous-button');
 
-        var validKey = validateKey(secretKey)
-        var validFile = validateFile(e)
-
-        if (validKey && validFile) {
-            encryptFile(secretKey, e)
+        switch (decision) {
+            case 'next':
+                showNext.style.display = 'block';
+                break;
+            case 'both':
+                showNext.style.display = 'block';
+                showPrev.style.display = 'block';
+                bottomNavDiv.style.justifyContent = 'space-between'
+                break
+            case 'none':
+                showNext.style.display = 'none';
+            default:
+                console.log('default hit');
         }
     }
 
-    function handleDecryption(e) {
-        const secretKey = enterSecretKeyRef.current.value;
-        var validKey = validateKey(secretKey);
-        var validFile = validateFile(e);
-        if (validKey && validFile) {
-            decryptFile(secretKey, e)
+    function handleTransition(e) {
+        var transitionType = e.target.id;
+        switch (currentStep) {
+            case 'password-input-wrapper':
+                var currentDiv = document.getElementById(currentStep);
+                var nextDiv = document.getElementById('file-upload-wrapper')
+                if (transitionType === 'show-next-button') {
+                    currentDiv.style.display = 'none';
+                    nextDiv.style.display = 'block';
+                    showTopNavigation(['encrypt-nav', 'decrypt-nav'])
+                    showBottomNavigation('both')
+                }
         }
     }
 
-    function validateKey(key) {
-        return key!== "" ? true : false
+    async function setFile(e) {
+        fileObj = e.target.files[0]
+        reader.readAsDataURL(fileObj)
+        reader.onload = await function (e) {
+            fileURL = reader.result
+        }
     }
 
-    function validateFile(e) {
-        const file = e.target.files[0]
-        return file !== "" ? true : false;
+    var makeEncryption = (e) => {
+        encrypted = CryptoJS.AES.encrypt(fileURL, 'password')
     }
 
-    function encryptFile(key, e) {
-        var file = e.target.files[0]
-        var reader = new FileReader();
-
-        reader.onload = (function (file) {
-            return function (e) {
-                var encrypted = CryptoAES.encrypt(e.target.result, key).toString();
-                encryptedFileResult = encrypted
-                showDownloadEncrypt()
-            }  
-        })(file)
-        reader.readAsText(file)
+    var downloadEncryptedFile = (e) => {
+        e.target.href = 'data:application/octet-stream,' + encrypted
+        e.target.download = fileObj.name + '.encrypted'
     }
 
-    function decryptFile(key, e) {
-        var file = e.target.files[0]
-        var reader = new FileReader();
-
-        reader.onload = (function (file) {
-            return function (e) {
-            var decrypted = CryptoAES.decrypt(e.target.result, key).toString(CryptoJS.enc.Latin1);
-                decryptedFileResult = decrypted
-                showDownloadDecrypt()
-            }  
-        })(file)
-        reader.readAsDataURL(file);
-    }
-
-    function showDownloadEncrypt() {
-        const encryptStep1 = encryptStep1Ref.current;
-        const encryptStep2 = encryptStep2Ref.current;
-        const encryptStep3 = encryptStep3Ref.current;
-        const encryptStep3Anchor = encryptStep3AnchorRef.current;
-
-        encryptStep1.style.display = "none";
-        encryptStep2.style.display = "none";
-        encryptStep3.style.display = "block"
-
-        encryptStep3Anchor.setAttribute("href", `data:application/octet-stream,${encryptedFileResult}`);
-        encryptStep3Anchor.setAttribute("download", "YourEncryptedFile.encrypted")
-    }
-
-    function showDownloadDecrypt() {
-        const decryptStep3 = decryptStep3Ref.current;
-        const decryptStep3Anchor = decryptStep3AnchorRef.current
-
-        decryptStep3.style.display = "block";
-        console.log(encryptedFileResult)
-        decryptStep3Anchor.setAttribute("href", `${decryptedFileResult}`)
-        decryptStep3Anchor.setAttribute("download", "YourDecryptedFile.png")
-
-
+    async function downloadDecryptedFile(e) {
+        reader.readAsText(fileObj)
+        reader.onload = await function (obj) {
+            decrypted = CryptoJS.AES.decrypt(obj.target.result, 'password').toString(CryptoJS.enc.Latin1)
+            e.target.href = decrypted;
+            e.target.download = fileObj.name.replace('.encrypted', '')
+        }
     }
 
     return (
         <div id="encrypter-applet-content-wrapper">
             <h2 className="encrypter-applet-content-title">AES Encrypter</h2>
-            <div ref={optionSelectRef} className="encrypter-applet-content-option-select">
-                <h3>Select an Option</h3>
-                <button onClick={handleEncryptButton}>Encrypt a File</button>
-                <button onClick={handleDecryptButton}>Decrypt a File</button>
+            <div id="encrypter-applet-navigation">
+                <button ref={ passwordNavRef } id="password-nav" className="encrypter-nav-button active">Password</button>
+                <button ref={ uploadNavRef } id="upload-nav"  className="encrypter-nav-button disabled">File Upload</button>
+                <button id="encrypt-nav"  className="encrypter-nav-button disabled">Encrypt</button>
+                <button id="decrypt-nav"  className="encrypter-nav-button disabled">Decrypt</button>
+                <button id="download-nav"  className="encrypter-nav-button disabled">File Download</button>
+            </div>
+            <div id="password-input-wrapper">
+                <h3 id="password-title">Enter your Secret Password</h3>
+                <input onChange={ (e) => setPassword(e) } id="password-input" type="text" />
             </div>
 
-            <div ref={encryptionOptionsRef} className="encryption-option-steps-wrapper">
-                <div ref={encryptStep1Ref} className="encrypt-1">
-                    <h3>Make a Secret Key</h3>
-                    <input ref={makeSecretKeyRef} required></input>
-                </div>
-
-                <div ref={encryptStep2Ref} className="encrypt-2">
-                    <h3>Open a File</h3>
-                    <input type="file" ref={browseFileEncryptRef} onChange={ (e) => handleEncryption(e)} />
-
-                </div>
-
-                <div ref={encryptStep3Ref} className="encrypt-3">
-                    <h3>Download your File</h3>
-                    <a ref={encryptStep3AnchorRef}>Download</a>
-                </div>
+            <div id="file-upload-wrapper">
+                <h3 id="file-upload-title">Upload a File</h3>
+                <input onChange={ (e) => setFile(e) } id="file-upload-input" type="file" />
+            </div>
+            
+            <div id="encrypt-decrypt-wrapper">
+                <h3 id="encrypt-decrypt-title">Encrypt or Decrypt</h3>
+                <button onClick={ () => makeEncryption() } id="encrypt">Encrypt</button>
+                <button id="decrypt">Decrypt</button>
             </div>
 
-            <div ref={decryptionOptionsRef} className="decryption-option-steps-wrapper">
-                <div ref={decryptStep1Ref} className="decrypt-1">
-                    <h3>Enter your Decryption Key</h3>
-                    <input ref={enterSecretKeyRef}></input>
-                </div>
-                <div ref={decryptStep2Ref} className="decrypt-2">
-                    <h3>Open a File</h3>
-                    <input type="file" ref={browseFileDecryptRef} onChange={ (e) => handleDecryption(e)} />
-                </div>
-                <div ref={decryptStep3Ref} className="decrypt-3">
-                    <h3>Download your File</h3>
-                    <a ref={decryptStep3AnchorRef}>Download</a>
-                </div>
+            <div id="download-wrapper">
+                <h3 id="download-title">Download your File</h3>
+                <a onClick={ (e) => downloadEncryptedFile(e) } href="#" className="download-link">Download Encrypted</a>
+                <a onClick={ (e) => downloadDecryptedFile(e) }  href="#" className="download-link">Download Decrypted</a>
             </div>
 
+            <div id="show-steps-wrapper">
+                <button id="show-previous-button">Previous</button>
+                <button id='show-next-button' onClick={(e) => handleTransition(e) }>Next</button>
+            </div>
         </div>
     )
 }
