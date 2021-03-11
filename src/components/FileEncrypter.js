bcbeef3947b0f6c9fd4dfcfa42fbee96ec115b53
JustinPaulJohnUA <justinpauljohn@email.arizona.edu>
2021-03-11 00:35:15 -0700

    file encrypter is finished. the style of the applet has been modified too. this applet should be refactored to take advantage of react-state and more testing should be conducted at some point too. for now, it is functional (aside from any potential edge-cases that I might have missed) and styled (for the most part).

--- src/components/FileEncrypter.js
+++ src/components/FileEncrypter.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React from 'react'
 
 /*SYTLE IMPORTS*/
 import '../styles/FileEncrypter.css'
@@ -6,163 +6,238 @@ import '../styles/FileEncrypter.css'
 /*COMPONENT IMPORTS */
 import CryptoJS from 'crypto-js'
 
+/* 
+This applet is functional, but it needs to be refactored badly and could 
+probably be re-written to take advantage of react-state in order to help 
+reduce the amount of lines. 
+
+Has been tested very slighly for edge-cases, but not extensively, so there 
+are probably some bugs that haven't been yet. 
+
+Either way, its functional and styled (for the most part), so I will consider
+refactoring and testing once everything else has been completed.
+*/
+
 export default function FileEncrypter() {
-    // TOP NAVIGATION
-    var passwordNavRef = useRef();
-    var uploadNavRef = useRef();
 
-    var currentStep = 'password-input-wrapper';
-    var availableNavs = [];
-
+    let decision = '';
+    var currentStep = 'encrypt-decrypt-wrapper';
     var password = '';
+    var key = '';
     var fileObj = '';
     var fileURL = '';
     var encrypted = '';
     var decrypted = '';
+    var fileToDecrypt = '';
 
     const reader = new FileReader();
 
     function setPassword(e) {
         password = String(e.target.value)
+        var btn = document.getElementById("encrypt-button");
+
         if (password.length > 0) {
-            showTopNavigation(['upload-nav'])
-            showBottomNavigation('next')
+            btn.style.display = "block"
         } else {
-            showTopNavigation([])
-            showBottomNavigation('none')
+            btn.style.display = 'none'
         }
-        
     }
 
-    function showTopNavigation(navs) {
-        if (navs.length === 0) {
-            console.log('nav length is 0')
-            var allnavs = ['upload-nav', 'encrypt-nav', 'decrypt-nav', 'download-nav']
-            allnavs.map(id => {
-                var navItem = document.getElementById(id);
-                var className = navItem.classList
-                if (!className.contains('disabled')) {
-                    navItem.classList.add('disabled')
-                    navItem.classList.remove('active')
-                }
-                
-            })
+    function setDecryptionKey(e) {
+        key = String(e.target.value)
+        var btn = document.getElementById('decrypt-button')
+        if (key.length > 0) {
+            btn.style.display = 'block'
         } else {
-            navs.map(id => {
-                var navItem = document.getElementById(id)
-                var className = navItem.classList;
-                if (!className.contains('active')) {
-                    className.remove('disabled')
-                    className.toggle('active')
-                } else if (className.contains('disabled')) {
-                    className.toggle('disabled')
-                    className.remove('active')
-                }
-                
-            })
-        }
-
-
-    }
-
-    function showBottomNavigation(decision) {
-        var bottomNavDiv = document.getElementById('show-steps-wrapper');
-        var showNext = document.getElementById('show-next-button');
-        var showPrev = document.getElementById('show-previous-button');
-
-        switch (decision) {
-            case 'next':
-                showNext.style.display = 'block';
-                break;
-            case 'both':
-                showNext.style.display = 'block';
-                showPrev.style.display = 'block';
-                bottomNavDiv.style.justifyContent = 'space-between'
-                break
-            case 'none':
-                showNext.style.display = 'none';
-            default:
-                console.log('default hit');
-        }
-    }
-
-    function handleTransition(e) {
-        var transitionType = e.target.id;
-        switch (currentStep) {
-            case 'password-input-wrapper':
-                var currentDiv = document.getElementById(currentStep);
-                var nextDiv = document.getElementById('file-upload-wrapper')
-                if (transitionType === 'show-next-button') {
-                    currentDiv.style.display = 'none';
-                    nextDiv.style.display = 'block';
-                    showTopNavigation(['encrypt-nav', 'decrypt-nav'])
-                    showBottomNavigation('both')
-                }
+            btn.style.display = 'none'
         }
     }
 
     async function setFile(e) {
-        fileObj = e.target.files[0]
-        reader.readAsDataURL(fileObj)
-        reader.onload = await function (e) {
-            fileURL = reader.result
+        if (e.target.files.length > 0) {
+            fileObj = e.target.files[0]
+            if (decision === 'decrypt') {
+                reader.readAsText(fileObj)
+                reader.onload = await function (obj) {
+                    fileToDecrypt = obj.target.result
+                    currentStep = 'decrypt-input-wrapper'
+                    showDecryptPassphrase()
+                }
+            } else if (decision === 'encrypt') {
+                reader.readAsDataURL(fileObj)
+                reader.onload = await function () {
+                    fileURL = reader.result
+                    currentStep = 'password-input-wrapper'
+                    showPassphrase();
+                }
+            }
+        } 
+    }
+
+    function showUpload(e) {
+        currentStep = 'file-upload-wrapper'
+        if (e.target.id === 'encrypt') {
+            decision = 'encrypt'
+        } else if (e.target.id === 'decrypt') {
+            decision = 'decrypt'
+        }
+
+        var encryptDecryptDiv = document.getElementById('encrypt-decrypt-wrapper')
+        var fileInputDiv = document.getElementById('file-upload-wrapper')
+
+        encryptDecryptDiv.style.display = 'none'
+        fileInputDiv.style.display = 'flex';
+        fileInputDiv.style.flexDirection = 'column';
+    }
+
+    function showPassphrase() {
+        currentStep = 'password-input-wrapper'
+        var uploadDiv = document.getElementById('file-upload-wrapper')
+        var passphraseDiv = document.getElementById('password-input-wrapper')
+
+        uploadDiv.style.display = 'none';
+        passphraseDiv.style.display = 'flex';
+        passphraseDiv.style.flexDirection = 'column';
+        passphraseDiv.style.alignItems = 'center'
+    }
+
+    function showDecryptPassphrase() {
+        currentStep = 'decrypt-input-wrapper'
+        var uploadDiv = document.getElementById('file-upload-wrapper')
+        var passphraseDecryptDiv = document.getElementById('decrypt-input-wrapper')
+
+        uploadDiv.style.display = 'none';
+        passphraseDecryptDiv.style.display = 'flex';
+        passphraseDecryptDiv.style.flexDirection = 'column';
+        passphraseDecryptDiv.style.alignItems = 'center'
+    }
+
+    function showEncryptionDownload() {
+        currentStep = 'download-wrapper'
+        var passphraseDiv = document.getElementById('password-input-wrapper')
+        var downloadDiv = document.getElementById('download-wrapper')
+        passphraseDiv.style.display = 'none';
+        downloadDiv.style.display = 'flex';
+        downloadDiv.style.flexDirection = 'column'
+        downloadDiv.style.alignItems = 'center'
+        downloadDiv.style.justifyContent = 'center'
+
+        makeEncryption()
+        downloadEncryptedFile()
+    }
+
+    function showDecryptionDownload() {
+        makeDecryption()
+        downloadDecryptedFile()
+
+        if(!/^data:/.test(decrypted)){
+            alert("Invalid pass phrase or file! Please try again.");
+            return false;
+        } else {
+            currentStep = 'download-wrapper'
+            var passphraseDiv = document.getElementById('decrypt-input-wrapper')
+            var downloadDiv = document.getElementById('download-wrapper')
+            passphraseDiv.style.display = 'none';
+            downloadDiv.style.display = 'flex';
+            downloadDiv.style.flexDirection = 'column'
+            downloadDiv.style.alignItems = 'center'
+            downloadDiv.style.justifyContent = 'center'
         }
     }
 
     var makeEncryption = (e) => {
-        encrypted = CryptoJS.AES.encrypt(fileURL, 'password')
+        encrypted = CryptoJS.AES.encrypt(fileURL, password)
+    }
+
+    var makeDecryption = (e) => {
+        console.log('decryption key is ', key)
+        decrypted = CryptoJS.AES.decrypt(fileToDecrypt, key).toString(CryptoJS.enc.Latin1)
+    }
+
+    function downloadFile() {
+        currentStep = 'download-wrapper'
+        if (decision === 'encrypt') {
+            var downloadButton = document.getElementById("download-link")
+            downloadButton.href = 'data:application/octet-stream,' + encrypted
+            downloadButton.download = fileObj.name + '.encrypted'
+        } else if (decision === 'decrypt') {
+            var downloadButton = document.getElementById("download-link")
+            downloadButton.href = decrypted;
+            downloadButton.download = fileObj.name.replace('.encrypted', '')
+        }
     }
 
     var downloadEncryptedFile = (e) => {
-        e.target.href = 'data:application/octet-stream,' + encrypted
-        e.target.download = fileObj.name + '.encrypted'
+        var downloadButton = document.getElementById("download-link")
+        downloadButton.href = 'data:application/octet-stream,' + encrypted
+        downloadButton.download = fileObj.name + '.encrypted'
     }
 
     async function downloadDecryptedFile(e) {
-        reader.readAsText(fileObj)
-        reader.onload = await function (obj) {
-            decrypted = CryptoJS.AES.decrypt(obj.target.result, 'password').toString(CryptoJS.enc.Latin1)
-            e.target.href = decrypted;
-            e.target.download = fileObj.name.replace('.encrypted', '')
-        }
+        var downloadButton = document.getElementById("download-link")
+        downloadButton.href = decrypted;
+        downloadButton.download = fileObj.name.replace('.encrypted', '')
+    }
+
+    function restartEncryption(e) {
+        var siblingWrapper = document.getElementById(currentStep);
+        var encryptDecryptDiv = document.getElementById("encrypt-decrypt-wrapper");
+
+        siblingWrapper.style.display = "none";
+        encryptDecryptDiv.style.display = "flex";
+        encryptDecryptDiv.style.flexDirection = "column";
+        encryptDecryptDiv.style.alignItems = "center";
+
+        document.getElementById('file-upload-input').value = null;
+
+        currentStep = 'encrypt-decrypt-wrapper'
     }
 
     return (
         <div id="encrypter-applet-content-wrapper">
-            <h2 className="encrypter-applet-content-title">AES Encrypter</h2>
-            <div id="encrypter-applet-navigation">
-                <button ref={ passwordNavRef } id="password-nav" className="encrypter-nav-button active">Password</button>
-                <button ref={ uploadNavRef } id="upload-nav"  className="encrypter-nav-button disabled">File Upload</button>
-                <button id="encrypt-nav"  className="encrypter-nav-button disabled">Encrypt</button>
-                <button id="decrypt-nav"  className="encrypter-nav-button disabled">Decrypt</button>
-                <button id="download-nav"  className="encrypter-nav-button disabled">File Download</button>
+            <div style={{display:"flex", justifyContent: "space-between"}}>
+                <h2 className="encrypter-applet-content-title">AES Encrypter</h2>
+                <button id="restartButton" style={{
+                    width: "100px",
+                    height: "40px",
+                    verticalAlign: "center",
+                    backgroundColor: "#3b3b98",
+                    border: "none",
+                    color: "white",
+                    borderRadius: "5px"
+                }} onClick={(e) => restartEncryption(e)}>Restart</button>
             </div>
-            <div id="password-input-wrapper">
-                <h3 id="password-title">Enter your Secret Password</h3>
-                <input onChange={ (e) => setPassword(e) } id="password-input" type="text" />
+            
+
+            <div id="encrypt-decrypt-wrapper" className="overlay">
+                <h3 id="encrypt-decrypt-title">Select an Option</h3>
+                <button onClick={ (e) => showUpload(e) } id="encrypt">Encrypt</button>
+                <button onClick={ (e) => showUpload(e) } id="decrypt">Decrypt</button>
             </div>
 
             <div id="file-upload-wrapper">
                 <h3 id="file-upload-title">Upload a File</h3>
-                <input onChange={ (e) => setFile(e) } id="file-upload-input" type="file" />
+                <input onChange={(e) => setFile(e)} id="file-upload-input" type="file" />
+            </div>
+
+            <div id="password-input-wrapper">
+                <h3 id="password-title">Enter your Secret Password</h3>
+                <input onChange={(e) => setPassword(e)} id="password-input" type="text" />
+
+                <button onClick={ (e) => showEncryptionDownload(e)} id="encrypt-button">Encrypt</button>
+            </div>
+
+            <div id='decrypt-input-wrapper'>
+                <h3>Enter your Decryption Key</h3>
+                <input onChange={(e) => setDecryptionKey(e)} id="decryption-key-input" type="text" />
+                <button onClick={(e) => showDecryptionDownload(e)} id="decrypt-button">Decrypt</button>
             </div>
             
-            <div id="encrypt-decrypt-wrapper">
-                <h3 id="encrypt-decrypt-title">Encrypt or Decrypt</h3>
-                <button onClick={ () => makeEncryption() } id="encrypt">Encrypt</button>
-                <button id="decrypt">Decrypt</button>
-            </div>
-
-            <div id="download-wrapper">
+            <div id="download-wrapper" className="overlay">
                 <h3 id="download-title">Download your File</h3>
-                <a onClick={ (e) => downloadEncryptedFile(e) } href="#" className="download-link">Download Encrypted</a>
-                <a onClick={ (e) => downloadDecryptedFile(e) }  href="#" className="download-link">Download Decrypted</a>
+                <a onClick={ (e) => downloadFile(e) } href="#" id="download-link">Download</a>
             </div>
 
-            <div id="show-steps-wrapper">
-                <button id="show-previous-button">Previous</button>
-                <button id='show-next-button' onClick={(e) => handleTransition(e) }>Next</button>
-            </div>
         </div>
     )
 }
